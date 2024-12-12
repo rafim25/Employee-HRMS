5c8d32f185274233a4711553022558090455d4e4
mohammad rafee <mohammad.rafee@piramal.com>
2024-12-12 19:11:28 +0530

    updated logo

--- Frontend/src/components/molecules/Form/FormDataPegawai/index.jsx
+++ Frontend/src/components/molecules/Form/FormDataPegawai/index.jsx
@@ -7,15 +7,15 @@ import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
 const FormDataPegawai = () => {
   return (
     <DefaultLayoutAdmin>
-      <BreadcrumbAdmin pageName='Form Employee' />
+      <BreadcrumbAdmin pageName='Employee Form' />
 
       <div className='sm:grid-cols-2'>
         <div className='flex flex-col gap-9'>
-          {/* <!-- Form Data Employee --> */}
+          {/* <!-- Employee Data Form --> */}
           <div className='rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark'>
             <div className='border-b border-stroke py-4 px-6.5 dark:border-strokedark'>
               <h3 className='font-medium text-black dark:text-white'>
-                Form Data Employee
+                Employee Data Form
               </h3>
             </div>
             <form action='#'>
@@ -23,22 +23,22 @@ const FormDataPegawai = () => {
                 <div className='mb-4.5 flex flex-col gap-6 xl:flex-row'>
                   <div className='w-full xl:w-1/2'>
                     <label className='mb-2.5 block text-black dark:text-white'>
-                      NIK <span className='text-meta-1'>*</span>
+                      CID <span className='text-meta-1'>*</span>
                     </label>
                     <input
                       type='number'
-                      placeholder='Masukkan nomor nik'
+                      placeholder='Enter Customer Id number'
                       className='w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
                     />
                   </div>
 
                   <div className='w-full xl:w-1/2'>
                     <label className='mb-2.5 block text-black dark:text-white'>
-                      Nama Lengkap <span className='text-meta-1'>*</span>
+                      Full Name <span className='text-meta-1'>*</span>
                     </label>
                     <input
                       type='text'
-                      placeholder='Masukkan nama lengkap'
+                      placeholder='Enter full name'
                       className='w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
                     />
                   </div>
@@ -51,7 +51,7 @@ const FormDataPegawai = () => {
                     </label>
                     <input
                       type='username'
-                      placeholder='Masukkan username'
+                      placeholder='Enter username'
                       className='w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
                     />
                   </div>
@@ -62,7 +62,7 @@ const FormDataPegawai = () => {
                     </label>
                     <input
                       type='password'
-                      placeholder='Masukkan password'
+                      placeholder='Enter password'
                       className='w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'
                     />
                   </div>
@@ -75,7 +75,7 @@ const FormDataPegawai = () => {
                     </label>
                     <div className='relative z-20 bg-transparent dark:bg-form-input'>
                       <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
-                        <option value=''>Silahkan pilih</option>
+                        <option value=''>Please select</option>
                         <option value=''>Male</option>
                         <option value=''>Female</option>
                       </select>
@@ -90,9 +90,9 @@ const FormDataPegawai = () => {
                     </label>
                     <div className='relative z-20 bg-transparent dark:bg-form-input'>
                       <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
-                        <option value=''>Silahkan Pilih</option>
+                        <option value=''>Please Select</option>
                         <option value=''>HRD</option>
-                        <option value=''>Staf Marketing</option>
+                        <option value=''>Marketing Staff</option>
                         <option value=''>Admin</option>
                         <option value=''>Sales</option>
                       </select>
@@ -119,7 +119,7 @@ const FormDataPegawai = () => {
                     </label>
                     <div className='relative z-20 bg-transparent dark:bg-form-input'>
                       <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
-                        <option value=''>Silahkan Pilih</option>
+                        <option value=''>Please Select</option>
                         <option value=''>Permanent Employee</option>
                         <option value=''>Contract Employee</option>
                       </select>
@@ -137,7 +137,7 @@ const FormDataPegawai = () => {
                     </label>
                     <div className='relative z-20 bg-transparent dark:bg-form-input'>
                       <select className='relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary'>
-                        <option value=''>Silahkan pilih</option>
+                        <option value=''>Please select</option>
                         <option value=''>Admin</option>
                         <option value=''>Employee</option>
                       </select>
@@ -157,12 +157,12 @@ const FormDataPegawai = () => {
                     />
                   </div>
                 </div>
-                {/* <!-- Form Data Employee --> */}
+                {/* <!-- Employee Data Form --> */}
 
                 <div className='flex flex-col md:flex-row w-full gap-3 text-center'>
                   <Link to="/" >
                     <ButtonOne  >
-                      <span>Simpan</span>
+                      <span>Save</span>
                     </ButtonOne>
                   </Link>
                   <Link to="/admin/master-data/data-pegawai/form-data-pegawai" >
@@ -172,7 +172,7 @@ const FormDataPegawai = () => {
                   </Link>
                   <Link to="/admin/master-data/data-pegawai" >
                     <ButtonThree  >
-                      <span>Kembali</span>
+                      <span>Back</span>
                     </ButtonThree>
                   </Link>
                 </div>
