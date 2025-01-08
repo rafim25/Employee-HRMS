e9dc444b309badec156c2d197b0c1a9c769ed47f
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-08 18:42:24 +0530

    bug fix

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -2,7 +2,8 @@ import { Sequelize } from 'sequelize';
 
 const db = new Sequelize('db_penggajian3', 'root', '', {
     host: "localhost",
-    dialect: "mysql"
+    dialect: "mysql",
+    dialectModule: require('mysql2'),
 });
 
 export default db;
