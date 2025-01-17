497a169372d308809c428a1ba8d1779d1e393265
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-18 01:25:57 +0530

    updated db name

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_penggajian3", "root", "Pas5word@1$3", {
+const db = new Sequelize("db_lending", "root", "Pas5word@1$3", {
   host: "localhost",
   dialect: "mysql",
 });
