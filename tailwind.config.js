141016be6c82060a0b890c0b708fe2714f666519
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-21 23:06:42 +0530

    added web site changes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", "serif"], // For the brand name
+      },
+      animation: {
+        "bounce-slow": "bounce 3s infinite",
+      },
+    },
+  },
+  // ... rest of your config
+};
