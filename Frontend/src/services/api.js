c5485ee338b295a2febb79f72ad9ed11d41e7a52
mohammad rafee <mohammad.rafee@piramal.com>
2025-01-28 12:29:49 +0530

    updated build

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3002";
 
-console.log("BASE_URL", BASE_URL);
+console.log("BASE_URL", import.meta.env);
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
