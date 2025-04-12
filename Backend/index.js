f4238f4d811f54692c19d7c0a680bbdf0c2a61c5
mohammad rafee <mohammadrafee@MacBook-Air.local>
2025-04-12 16:43:40 +0530

    updated index.js

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-dotenv.config();
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+// const envPath = path.resolve(process.cwd(), envFile);
+
+console.log(`📦 Loaded ENV: ${envFile}`);
+console.log(`🔧 Current Mode: ${process.env.NODE_ENV}`);
+console.log(`🎯 DB Host: ${process.env.DB_HOST}`);
 
 // CORS middleware
 app.use(cors({
@@ -44,12 +51,7 @@ app.use(cors({
 // JSON middleware
 app.use(express.json());
 
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
-// const envPath = path.resolve(process.cwd(), envFile);
 
-console.log(`📦 Loaded ENV: ${envFile}`);
-console.log(`🔧 Current Mode: ${process.env.NODE_ENV}`);
-console.log(`🎯 DB Host: ${process.env.DB_HOST}`);
 
 // Health Check Endpoint
 app.get('/api/health', async (req, res) => {
