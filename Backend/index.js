b6fdcd19665b0ae356a93c65e2aba7b842ba8ce3
mohammad rafee <mohammadrafee@MacBook-Air.local>
2025-06-11 14:03:42 +0530

    added cors endpoint

--- Backend/index.js
+++ Backend/index.js
@@ -52,10 +52,12 @@ const app = express();
 
 // Define allowed origins
 const allowedOrigins = [
+  'https://sevenwingstechnologies.in/',
   'http://localhost:5173',
   'http://13.60.189.178',
   'http://13.60.189.178:3002',
-  'http://13.60.189.178:5173'
+  'http://13.60.189.178:5173',
+  'https://sevenwingstechnologies.in/api'
 ];
 
 // CORS middleware
